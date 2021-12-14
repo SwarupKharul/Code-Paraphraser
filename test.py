34e677e49c632c393abe308bdb7dfe962d3016a7
Swarup Kharul <swarupkharulsk@gmail.com>
2021-12-14 10:52:15 +0530

    feat: add .gitignore

--- test.py
+++
@@ -1,7 +0,0 @@
-# This is A comment
-def example_function(A = 60, param2 = 50):
-    """ this is A multiline
-    docstring for testing """
-    string = " this is A string "
-    print(A)
-    print(param2)
